1c097ef1ad37809a81520a36162a599eaabdb4d0
KomradOl <o.akinshin@gmail.com>
2019-08-11 09:24:51 +0400

    #7

--- gulpfile.js
+++ gulpfile.js
@@ -10,14 +10,33 @@ var server = require("browser-sync").create();
 var csso = require("gulp-csso");
 var rename = require("gulp-rename");
 var del = require("del");
-var posthtml = require("posthtml-include");
 var imagemin = require("gulp-imagemin");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
 
-gulp.task("clean", function () {
+gulp.task("sprite", function() {
+  return gulp.src("source/img/logo-pink-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("html", function() {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function() {
   return del("build");
 });
 
-gulp.task("css", function () {
+gulp.task("css", function() {
   return gulp.src("source/less/style.less")
     .pipe(plumber())
     .pipe(sourcemap.init())
@@ -32,20 +51,19 @@ gulp.task("css", function () {
     .pipe(server.stream());
 });
 
-gulp.task("copy", function () {
+gulp.task("copy", function() {
   return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
-    "source/img/**",
-    "source/js/**",
-    "source/*.html"
-],{
-  base:"source"
-})
-.pipe(gulp.dest("build"));
+      "source/fonts/**/*.{woff,woff2}",
+      "source/img/**",
+      "source/js/**"
+    ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
 });
 
 
-gulp.task("server", function () {
+gulp.task("server", function() {
   server.init({
     server: "build/",
     notify: false,
@@ -58,5 +76,6 @@ gulp.task("server", function () {
   gulp.watch("source/*.html").on("change", server.reload);
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css"));
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite",
+  "html"));
 gulp.task("start", gulp.series("build", "server"));
--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,7 @@ var navHeader = document.querySelector('.page-header__nav-list');
 var buttonList = document.querySelector('.button-list__toggle');
 var buttonOn = document.querySelector('.button-list__toggle-open');
 var buttonOff = document.querySelector('.button-list__toggle-close');
+var buttonClosed = document.querySelector('.button-list');
 
 navHeader.classList.remove('page-header__nav-list--nojs');
 buttonList.classList.remove('button-list__toggle--nojs');
@@ -14,13 +15,14 @@ buttonOn.addEventListener('click', function() {
     buttonOff.classList.add('button-list__toggle-close--opened');
     buttonOn.classList.remove('button-list__toggle-close--opened');
     buttonOn.classList.add('button-list__toggle-close--closed');
+    buttonClosed.classList.remove('button-list--closed');
   } else {
-    navHeader.classList.add('page-header__nav-list--closed');
     navHeader.classList.add('page-header__nav-list--opened');
     buttonOff.classList.remove('button-list__toggle-close--closed');
     buttonOff.classList.add('button-list__toggle-close--opened');
     buttonOn.classList.remove('button-list__toggle-close--opened');
     buttonOn.classList.add('button-list__toggle-close--closed');
+    buttonClosed.classList.remove('button-list--closed');
   }
 });
 
@@ -32,6 +34,7 @@ buttonOff.addEventListener('click', function() {
     buttonOff.classList.add('button-list__toggle-close--closed');
     buttonOn.classList.remove('button-list__toggle-close--closed');
     buttonOn.classList.add('button-list__toggle-close--opened');
+    buttonClosed.classList.add('button-list--closed');
   } else {
     navHeader.classList.add('page-header__nav-list--opened');
     navHeader.classList.remove('page-header__nav-list--closed');
@@ -39,5 +42,6 @@ buttonOff.addEventListener('click', function() {
     buttonOff.classList.add('button-list__toggle-close--closed');
     buttonOn.classList.remove('button-list__toggle-close--closed');
     buttonOn.classList.add('button-list__toggle-close--opened');
+    buttonClosed.classList.add('button-list--closed');
   }
 });
